8ce4f0de0c9526c843eb86a4df8323c00a046bfc
An Jung Ha <ajh5909@nate.com>
2016-01-12 23:52:00 +0900

    chatting modified2

--- 201221427/client/memo.js
+++ 201221427/client/memo.js
@@ -1,6 +1,6 @@
-Router.route('/chatting', 'chatting');
+Router.route('/memo', 'memo');
 
-Template.chatting.events({
+Template.memo.events({
     'keyup [name=send]': function(evt, tmpl) {
         if (evt.which === 13) {
             var message = $("[name=send]").val();
@@ -12,7 +12,7 @@ Template.chatting.events({
     }
 });
 
-Template.chatting.helpers({
+Template.memo.helpers({
     messages: function() {
         var chattings = Chatting.find().fetch();
         chattings = chattings.reverse();
--- 201221427/lib/collections.js
+++ 201221427/lib/collections.js
@@ -1,5 +1,5 @@
 //Students = new Meteor.Collection('students');
-Chatting = new Meteor.Collection('chatting');
+Chatting = new Meteor.Collection('memo');
 Boards = new Meteor.Collection('boards');
 // 필요한 액션이 실시(게시판 글작성) 될 때 해당 액션을 insert하고
 // 필요할때 꺼내 쓴다.
