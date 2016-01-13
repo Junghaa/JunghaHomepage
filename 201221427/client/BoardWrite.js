624fc5e487473d66ae802219f4ffef81c01f5b4d
An Jung Ha <ajh5909@nate.com>
2016-01-13 23:55:26 +0900

    only clear button

--- 201221427/client/BoardWrite.js
+++ 201221427/client/BoardWrite.js
@@ -9,24 +9,24 @@ Template.BoardWrite.events({
         $('#본문').val('');
     },
     "click #write": function(e, tmpl) {
-        /*if (!Meteor.user()) {
+        if (!Meteor.user()) {
             return alert('로그인을 해주세요!!!');
-        }*/
+        }
         var obj = {};
         console.log('#본문');
         obj.작성자 = Meteor.user().emails[0].address;
         obj.제목 = $('#제목').val();
-        /*if(obj.작성자.length <= 0 || obj.제목.length <= 0) {
+        if(obj.작성자.length <= 0 || obj.제목.length <= 0) {
             //error
             return alert('작성자와 제목을 모두 입력해주세요!!');
-        }*/
+        }
         obj.본문 = $('#본문').val();
         console.log('#본문');
 
         Boards.insert(obj);
-        /*obj.createdAt = new Date();
+        obj.createdAt = new Date();
         obj.type = 'boardWriting';
-        Logs.insert(obj);*/
+        Logs.insert(obj);
         console.log('#본문');
         $('#작성자').val('');
         $('#제목').val('');
